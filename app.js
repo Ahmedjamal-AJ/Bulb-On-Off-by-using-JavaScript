ea395a3c9e9d8e48f9add9c793c3f6ce68f1aea1
Ahmedjamal-AJ <154009661+Ahmedjamal-AJ@users.noreply.github.com>
2023-12-17 22:30:37 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function bulbOn(){
+    let bulbobj = document.querySelector(".Bulboff");
+    bulbobj.setAttribute("src", "Assets/bulb on.jpg");
+}
+
+function bulbOff(){
+    let bulbobj = document.querySelector(".Bulboff");
+    bulbobj.setAttribute("src", "Assets/bulb off.jpg");
+}
